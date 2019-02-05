47450f3e1796c0810ff90110655749f33a1c2513
Vapurrmaid <vapurrmaid@gmail.com>
2019-02-05 17:09:50 -0500

    Whitelist Actuator

--- run-dev.sh
+++ run-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./mvnw spring-boot:run -Dspring-boot.run.arguments=--allowLocalhost=true
+./mvnw spring-boot:run -Dspring-boot.run.arguments=--allowLocalhost=true,--management.endpoints.web.cors.allowed-origins=http://localhost:8081,--management.endpoints.web.cors.allowed-methods=GET
 
